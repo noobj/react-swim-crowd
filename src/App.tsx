ce0b72a01650a6be207979dff16a3e9f847190c2
noobj <jingfu99@gmail.com>
2022-04-19 16:22:05 +0800

    refactor: replace useState by useReducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,35 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useReducer, useState } from 'react';
 import { format, subDays } from 'date-fns';
 import ControlPanel from './components/ControlPanel';
 import { MainContent } from './components/MainContent';
 
+const initialState = {
+  start: format(subDays(new Date(), 3), 'yyyy-MM-dd HH:mm'),
+  end: format(new Date(), 'yyyy-MM-dd HH:mm')
+};
+
+function reducer(state: {start: string, end: string}, action: {isStart: boolean, value: string}) {
+  switch (action.isStart) {
+    case true:
+      return {...state, start: action.value};
+    case false:
+      return {...state, end: action.value};
+    default:
+      return state;
+  }
+}
+
 export function App() {
-  const [start, setStart] = useState(format(subDays(new Date(), 3), 'yyyy-MM-dd HH:mm'));
-  const [end, setEnd] = useState(format(new Date(), 'yyyy-MM-dd HH:mm'));
+  const [state, dispatch] = useReducer(reducer, initialState);
+  // const [start, setStart] = useState(format(subDays(new Date(), 3), 'yyyy-MM-dd HH:mm'));
+  // const [end, setEnd] = useState(format(new Date(), 'yyyy-MM-dd HH:mm'));
   const [response, setResponse] = useState([]);
 
   useEffect(() => {
     async function fetchEntries() {
       const params = new URLSearchParams();
-      params.set('start', start);
-      params.set('end', end);
+      params.set('start', state.start);
+      params.set('end', state.end);
       const backendUrl = 'https://04xkzdmmq6.execute-api.ap-southeast-1.amazonaws.com/crowd/daily';
 
       const res = await fetch(`${backendUrl}?${params.toString()}`);
@@ -32,16 +49,15 @@ export function App() {
     }
 
     fetchContent();
-  }, [start, end]);
+  }, [state]);
 
   const changeHandler = async (isStart: boolean, dateStr: string) => {
-    if (isStart) setStart(dateStr);
-    else setEnd(dateStr);
+    dispatch({ isStart, value: dateStr})
   };
 
   return (
     <>
-      <ControlPanel start={start} end={end} changeHandler={changeHandler} />
+      <ControlPanel start={state.start} end={state.end} changeHandler={changeHandler} />
       <MainContent data={response}/>
     </>
   );
